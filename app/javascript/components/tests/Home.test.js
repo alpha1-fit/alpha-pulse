bf8becaad3c0b405cf7e8c43cf68d0dd4bbcfd34
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-04-20 10:51:04 -0700

    added back home.test.js and restored to original test

--- app/javascript/components/tests/Home.test.js
+++ app/javascript/components/tests/Home.test.js
@@ -10,9 +10,6 @@ describe("<Home />", () => {
         <BrowserRouter>
           <Home />
         </BrowserRouter>)
-        expect(screen.getByText(
-          /at alpha1, we are passionate about helping you achieve your overall health, not just your physical health\. we believe that working together and staying committed is the key to success\. that's why we created alphapulse, the fitness app that connects you to your friends and family's workouts\. with alphapulse, you can work out with your loved ones no matter where they are in the world, creating a sense of social interaction and accountability\. challenge yourself and others, and log your sessions to track your progress\. alphapulse empowers everyone to pursue their fitness goals\. join us on our shared fitness journey today\./i
-          ))
         expect(screen.getByRole('heading', {
           name: /are you tired of struggling to achieve your fitness goals alone\?/i
         }))
