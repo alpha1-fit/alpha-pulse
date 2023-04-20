96a9d178b4552b5bde348124f84955c0aa27a5c8
JoelCarr619 <JoelCarr619@gmail.com>
2023-04-20 13:05:28 -0700

    removed commented code

--- app/javascript/components/tests/NotFound.test.js
+++ app/javascript/components/tests/NotFound.test.js
@@ -1,23 +1,3 @@
-// import React from "react";
-// import NotFound from "../pages/NotFound";
-// import { BrowserRouter } from "react-router-dom";
-// import { render, screen } from "@testing-library/react";
-
-// describe("<NotFound />", () => {
-//   it("displays text telling my user the page is not found", () => {
-//     render(
-//       <BrowserRouter>
-//         <NotFound />
-//       </BrowserRouter>
-//     );
-//     screen.logTestingPlaygroundURL();
-//     expect(
-//       screen.getByRole("heading", {
-//         name: /Page Not Found/i,
-//       })
-//     ).toBeInTheDocument;
-//   });
-// });
 import { render, screen } from "@testing-library/react";
 import NotFound from "../pages/NotFound";
 import { BrowserRouter } from "react-router-dom";
