78c448bf89e303437a4685afae7819fa2dcbb45d
yiahmed <yahyahmed28@gmail.com>
2023-04-24 13:24:57 -0700

    completed updated functionality and testing (frontend & backend)

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,5 +3,19 @@ class WorkoutsController < ApplicationController
         workouts = Workout.all
         render json: workouts
     end
+
+    def update
+        workouts = Workout.find(params[:id])
+        workout.update(workout_params)
+        if workout.valid?
+            render json: workout
+        else
+            render json: workout.errors, status: 422
+        end
+    end
+
+    private
+    def workout_params
+      params.require(:workout).permit(:name, :workout_type, :duration, :schedule, :description, :user_id)
+    end
 end
-     
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -25,6 +25,19 @@ const App = (props) => {
     .then((payload) => setWorkouts(payload))
     .catch((error) => console.log(error))
   }
+
+  const updateWorkout = (workout, id) => {
+    fetch(`/workouts/${id}`, {
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+    })
+      .then((response) => response.json())
+      .then((payload) => readWorkouts(payload))
+      .catch((errors) => console.log("workout update errors:", errors));
+  }
   return (
     <div className='page'>
       <BrowserRouter>
@@ -35,7 +48,7 @@ const App = (props) => {
           <Route path='/workoutindex' element={<IndexWorkouts  {...props} workouts={workouts}/>} />
           <Route path='/workoutnew/new' element={<CreateWorkout />} />
           <Route path='/workoutshow/:id' element={<ShowWorkout />} />
-          <Route path='/workoutedit/:id/edit' element={<EditWorkout />} />
+          <Route path='/workoutedit/:id/edit' element={<EditWorkout workouts={workouts} updateWorkout={updateWorkout}/>} />
           <Route path='/commentindex' element={<IndexComments />} />
           <Route path='/commentnew' element={<CreateComment />} />
           <Route path='/commentedit/:id' element={<EditComment />} />
