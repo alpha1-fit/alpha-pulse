406857f67c2617899302ab44385a54a47772a3a3
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-04-25 11:49:04 -0700

    Fixed syntax changes showed the files love

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,67 +1,74 @@
-import React, {useState} from 'react'
-import { Nav,
+import React, { useState } from "react";
+import {
+  Nav,
   NavItem,
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
-  NavbarText,} from 'reactstrap'
-import { NavLink } from 'react-router-dom';
-import alphaPulseLogo from '../assets/PSX_20230414_155911- 1.png'
+  NavbarText,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import alphaPulseLogo from "../assets/PSX_20230414_155911- 1.png";
 const Header = ({
   logged_in,
   current_user,
   new_user_route,
   sign_in_route,
-  sign_out_route
+  sign_out_route,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
-    // <div className='Header'>
-    <Navbar className='header' fixed="top" expand color="dark" dark>
+    <Navbar className="header" fixed="top" expand color="dark" dark>
       <a href="/">
-        <NavItem ><img src={alphaPulseLogo} alt='AlphaPulse Logo' style={{height: '50px',  width: '50px'}}/></NavItem>
+        <NavItem>
+          <img
+            src={alphaPulseLogo}
+            alt="AlphaPulse Logo"
+            style={{ height: "50px", width: "50px" }}
+          />
+        </NavItem>
       </a>
-        <NavbarBrand href="/">AlphaPulse</NavbarBrand>
-        {/* <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar> */}
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink to="/workoutindex">Workouts</NavLink>
-            </NavItem>
-            {logged_in && (
-              <>
-              <div className='navitem'>
+      <NavbarBrand href="/">AlphaPulse</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="me-auto" navbar>
+          <NavItem>
+            <NavLink to="/workoutindex">Workouts</NavLink>
+          </NavItem>
+          {logged_in && (
+            <>
+              <div className="navitem">
                 <NavItem>
                   <NavLink to="/workoutnew/new">Create Workouts</NavLink>
                 </NavItem>
-                </div>
-                <div className='navitem'>
+              </div>
+              <div className="navitem">
                 <NavItem>
                   <a href={sign_out_route}>Sign Out</a>
                 </NavItem>
-                </div>
-              </>
-            )}
-            {!logged_in && (
-              <>
-              <div className='navitem'>
+              </div>
+            </>
+          )}
+          {!logged_in && (
+            <>
+              <div className="navitem">
                 <NavItem>
                   <a href={sign_in_route}>Sign In</a>
                 </NavItem>
-                </div>
-                <div className='navitem'>
+              </div>
+              <div className="navitem">
                 <NavItem>
                   <a href={new_user_route}>Sign Up</a>
                 </NavItem>
-                </div>
-              </>
-            )}
-          </Nav>
-        {/* </Collapse> */}
-      </Navbar>
-    // </div>
-  )
-}
-export default Header
+              </div>
+            </>
+          )}
+        </Nav>
+      </Collapse>
+    </Navbar>
+    
+  );
+};
+export default Header;
--- app/javascript/components/pages/ShowWorkout.js
+++ app/javascript/components/pages/ShowWorkout.js
@@ -1,91 +1,74 @@
-import React from "react"
-import {Card, CardBody, CardSubtitle, CardText, CardTitle, Button} from 'reactstrap'
-import { NavLink, useParams, useNavigate } from "react-router-dom"
-import fakeWorkouts from '../fakeWorkouts'
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Button,
+} from "reactstrap";
+import { NavLink, useParams, useNavigate } from "react-router-dom";
+import fakeWorkouts from "../fakeWorkouts";
 
 const ShowWorkout = ({ workouts, logged_in }) => {
-  const { id } = useParams()
-  const navigate = useNavigate()
-  let selectedWorkout = Array.isArray(workouts) && workouts.find(
-    (workout) => workout.id === +id
-  );
-  let fakeSelectedWorkout = Array.isArray(fakeWorkouts) && fakeWorkouts.find(
-    (fakeWorkout) => fakeWorkout.id === +id)
-  
-  
+  const { id } = useParams();
+  const navigate = useNavigate();
+  let selectedWorkout =
+    Array.isArray(workouts) && workouts.find((workout) => workout.id === +id);
+  let fakeSelectedWorkout =
+    Array.isArray(fakeWorkouts) &&
+    fakeWorkouts.find((fakeWorkout) => fakeWorkout.id === +id);
+
   return (
-    <div className='workout-show-align'>
-    {selectedWorkout &&(
-      <Card
-      className='workout-show'
-      style={{
-        width: '25%',
-        marginTop: '0px'
-      }}
-    >
-      <CardBody>
-        <CardTitle tag="h5">
-          {selectedWorkout.name}
-        </CardTitle>
-        <CardSubtitle
-          className="mb-2 text-muted"
-          tag="h6"
+    <div className="workout-show-align">
+      {selectedWorkout && (
+        <Card
+          className="workout-show"
+          style={{
+            width: "25%",
+            marginTop: "0px",
+          }}
         >
-          Workout_type: {selectedWorkout.workout_type}
-        </CardSubtitle>
-        <CardText>
-          Duration: {selectedWorkout.duration}
-        </CardText>
-        <CardText>
-          Schedule: {selectedWorkout.schedule}
-        </CardText>
-        <CardText>
-          Description: {selectedWorkout.description}
-        </CardText>
-      <Button><NavLink to={`/WorkoutEdit/${selectedWorkout.id}`} className="nav-link">
-          Edit a Workout
-          </NavLink></Button>
-     
-      </CardBody>
-    </Card>
-    )}
-
-{!logged_in && (
-  
-    <Card
-        className='workout-show'
-        style={{
-            width: '25%',
-            marginTop: '0px'
-        }}
-    >
-        <CardBody>
-            <CardTitle tag="h5">
-                {fakeSelectedWorkout.name}
-            </CardTitle>
-            <CardSubtitle
-                className="mb-2 text-muted"
-                tag="h6"
-            >
-                Workout_type: {fakeSelectedWorkout.workout_type}
+          <CardBody>
+            <CardTitle tag="h5">{selectedWorkout.name}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Workout_type: {selectedWorkout.workout_type}
             </CardSubtitle>
-            <CardText>
-                Duration: {fakeSelectedWorkout.duration}
-            </CardText>
-            <CardText>
-                Schedule: {fakeSelectedWorkout.schedule}
-            </CardText>
-            <CardText>
-                Description: {fakeSelectedWorkout.description}
-            </CardText>
-             {/* <Button><NavLink to={`/WorkoutEdit/${fakeSelectedWorkout.id}`} className="nav-link">
-                    Edit a Workout
-                </NavLink></Button> */}
-        </CardBody>
-    </Card>
-)}
-  </div>
-  )
-}
+            <CardText>Duration: {selectedWorkout.duration}</CardText>
+            <CardText>Schedule: {selectedWorkout.schedule}</CardText>
+            <CardText>Description: {selectedWorkout.description}</CardText>
+            <Button>
+              <NavLink
+                to={`/WorkoutEdit/${selectedWorkout.id}`}
+                className="nav-link"
+              >
+                Edit a Workout
+              </NavLink>
+            </Button>
+          </CardBody>
+        </Card>
+      )}
+      {!logged_in && (
+        <Card
+          className="workout-show"
+          style={{
+            width: "25%",
+            marginTop: "0px",
+          }}
+        >
+          <CardBody>
+            <CardTitle tag="h5">{fakeSelectedWorkout.name}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Workout_type: {fakeSelectedWorkout.workout_type}
+            </CardSubtitle>
+            <CardText>Duration: {fakeSelectedWorkout.duration}</CardText>
+            <CardText>Schedule: {fakeSelectedWorkout.schedule}</CardText>
+            <CardText>Description: {fakeSelectedWorkout.description}</CardText>
+          </CardBody>
+        </Card>
+      )}
+    </div>
+  );
+};
 
-export default ShowWorkout
+export default ShowWorkout;
