4aaca6716ad92e10bcbd324b8e3d476e0a2a6892
csaument <c.s.aument@gmail.com>
2023-04-25 20:35:32 -0700

    creates a form to create comments, form uses a callback function provided via props to access the fetch operation to the backend, creates a unit test for the form, test ensures presence of each element, test ensures submit button accesses the callback function, test ensures the submit and cancel buttons call useNavigate Hook

---
+++ app/javascript/_tests_/CreateComment.test.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CreateComment from "../components/pages/CreateComment";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+describe("<CreateComment />", () => {
+  const mockComment = {
+    title: "Terminator",
+    comment: "Ill be back...",
+    workout_id: 1,
+    user_id: 1
+  }
+
+  const mockUser1 = {
+    email: 'test@testing1.com',
+    password: 'testing123',
+    password_confirmation: 'testing123',
+    username: 'Dennis',
+    photo: 'url',
+    id: 1
+  }
+
+  const createCommentSpy = jest.fn()
+
+  const mockUseNavigate = jest.fn()
+
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockUseNavigate,
+  }))
+
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <CreateComment createComment={createCommentSpy} logged_in={true} current_user={mockUser1} workout_id={1} />
+      </BrowserRouter>
+    )
+  })
+
+  it("renders the page", () => {
+  })
+
+  it("has a form with entries for title and comment", () => {
+    expect(screen.getByRole('textbox', {
+      name: /title/i
+    }))
+      .toBeInTheDocument
+
+    expect(screen.getByRole('textbox', {
+      name: /comment/i
+    }))
+      .toBeInTheDocument
+  })
+
+  it("has a button to submit", () => {
+    expect(
+      screen.getByRole("button", {
+        name: /submit/i,
+      })
+    ).toBeInTheDocument
+  })
+
+  it("has a button to cancel", () => {
+    expect(
+      screen.getByRole("button", {
+        name: /cancel/i,
+      })
+    ).toBeInTheDocument
+  })
+
+  it("submits data on button click", () => {
+    let confirmButton = screen.getByRole("button", {
+      name: /submit/i,
+    })
+    userEvent.click(confirmButton)
+
+    expect(createCommentSpy).toHaveBeenCalled
+    expect(mockUseNavigate).toHaveBeenCalled
+  })
+
+  it("returns to the index on cancel click", () => {
+    let cancelButton = screen.getByRole("button", {
+      name: /cancel/i,
+    })
+    userEvent.click(cancelButton)
+
+    expect(mockUseNavigate).toHaveBeenCalled
+  })
+})
--- app/javascript/components/pages/CreateComment.js
+++ app/javascript/components/pages/CreateComment.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from "react"
+import { FormGroup, Label, Input, Form,Button } from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
-const CreateComment = () => {
+const CreateComment = ({logged_in, current_user, workout_id, CreateComment}) => {
+  const navigate = useNavigate()
+  const [newComment, setNewComment] = useState({
+    title: "",
+    comment: "",
+    workout_id: "",
+    user_id: current_user.id
+  })
+
+  const handleChange = (e) => {
+    setNewComment({...newComment, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = () => {
+    createComment(newComment)
+    navigate(`/WorkoutShow/${workout_id}`)
+  }
+
+  const cancelCreate = () => {
+    navigate(`/WorkoutShow/${workout_id}`)
+  }
+
   return (
-    <div>CreateComment</div>
+    <div className="comment-section">
+      <Form>
+        <FormGroup>
+          <Label htmlFor="title">Title</Label>
+          <Input id="title" name="title" placeholder="What is the comement title?" type="text" required={true} onChange={handleChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="comment">Comment</Label>
+          <Input id="comment" name="comment" placeholder="What is the comement?" type="textarea" required={true} onChange={handleChange} />
+        </FormGroup>
+        <Button onClick={handleSubmit} name="submit">
+              Submit
+        </Button>
+        <Button onClick={cancelCreate} name="cancel">
+              Cancel
+        </Button>
+      </Form>
+    </div>
   )
 }
 
