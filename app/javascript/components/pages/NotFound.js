17264334300e8e2f937afb9ebe86eff5482e7d1c
JoelCarr619 <JoelCarr619@gmail.com>
2023-05-01 09:36:22 -0700

    gave motion to the not found page

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const NotFound = () => {
   return (
     <div className="page-body">
@@ -7,9 +6,15 @@ const NotFound = () => {
       <img
         className="alpha pulse"
         src="https://external-preview.redd.it/harry-squatter-the-boy-who-lifted-v0-uDkvOZYhn0hXjCIQjDYw3g0X3PvqOaAKVpe85PmNDGg.png?format=pjpg&auto=webp&s=adeaa5997bd6c8ae72b55fcabd5e94ef51143ad5"
+        style={{
+          width: "50%",
+          height: "auto",
+          display: "block",
+          margin: "0 auto",
+          animation: "pulse 2s infinite",
+        }}
       />
     </div>
   );
 };
-
 export default NotFound;
